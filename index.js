70ca075900c46be76d1d7e5b5a21162fb72b54cf
Mehtashaab <mehtaabhishek499@gmail.com>
2024-09-19 15:42:25 +0530

    step1: install the express and expres-handlebars module fomr the npm manager step2: setup the file structure,Create the Views folder for storing the handlebars html and in views folder also create the layeout folder for storing the main hanadlers file step3: Create the html for home,about,contact page and alo created the routes for that Step4: Create the gitignore file

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { create } from "express-handlebars";
+
+const app = express();
+
+// Set up template engine
+const hbs = create({ defaultLayout: "main" });
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+    res.render("index",{title:"Home"});
+});
+
+app.get("/about", (req, res) => {
+    res.render("about",{title:"About"});
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact",{title:"contact"});
+});
+app.listen(8000, () => {
+    console.log("Server started on port 8000");
+});
